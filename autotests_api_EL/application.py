5c3e82dcd166243cbe768ce5e283256fd8a1ead3
LakhtionovEP <69114898+LakhtionovEP@users.noreply.github.com>
2021-12-15 21:37:24 +0300

    Update application.py

--- autotests_api_EL/application.py
+++ autotests_api_EL/application.py
@@ -1,1 +1,10 @@
+from clients.pet import ApiPetStore
+from helpers.message import MessageHelper
+from checkers.general import Checker
 
+
+class Application:
+    def __init__(self):
+        self.api = ApiPetStore()
+        self.helper = MessageHelper()
+        self.checker = Checker()
