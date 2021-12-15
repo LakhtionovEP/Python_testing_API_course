4ae8c04138d5076665dbc83258a70519c417412e
LakhtionovEP <69114898+LakhtionovEP@users.noreply.github.com>
2021-12-15 22:06:07 +0300

    2

--- autotests_api_EL/conftest.py
+++ autotests_api_EL/conftest.py
@@ -1,1 +1,11 @@
+import pytest
 
+from application import Application
+from helpers.message import MessageHelper
+
+fixture = Application()
+
+
+@pytest.fixture(scope="session")
+def base_fixture():
+    return fixture
