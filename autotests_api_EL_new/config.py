3b8ffef1f96fbbb96b6f1625578e349fbaf021ba
LakhtionovEP <69114898+LakhtionovEP@users.noreply.github.com>
2021-12-23 16:45:58 +0300

    Delete config.py

--- autotests_api_EL_new/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    url = 'http://survey-back-admin-bff-eco-services-stage.apps.okd.stage.digital.rt.ru/'
-    token = '8987-ttblnd28fuec-19r8nenc42foe16'
-    login = 'admin'
-    password = 'pas23Wd1'
